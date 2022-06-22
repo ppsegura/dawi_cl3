ffd2af8760f0934967d1d738d8f5eeb0145b0aa6
pzpenas <pedro.pe.segura@gmail.com>
2022-06-22 01:20:01 -0500

    inicio

---
+++ src/main/java/com/cibertec/DawiCl3PsegurapedroApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3PsegurapedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3PsegurapedroApplication.class, args);
+	}
+
+}
